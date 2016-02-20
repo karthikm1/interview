afd577690c3dcca521653b5031bc7fc8e0ce2e63
karthikm1 <karthikm1@outlook.com>
2016-02-20 14:08:39 -0800

    Create twoSumHashTable.cs

---
+++ twoSumHashTable.cs
@@ -0,0 +1,26 @@
+
+// Two Sum problem with hashtable
+public int[] twoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+
+            Dictionary<int,int> set = new Dictionary<int,int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int x = target - nums[i];
+                if (set.ContainsKey(x))
+                {
+                    result[0] = set[x];
+                    result[1] = i;
+                    return result;
+                }
+                else
+                {
+                    set.Add(nums[i], i);
+                }
+            
+            }
+            return result;
+        
+        }
